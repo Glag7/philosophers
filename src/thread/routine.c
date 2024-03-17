e80cd97ee479361792d8f5972f501087d36ed949
Glag7 <glaguyonie@gmail.com>
2024-03-17 18:53:04 +0100

    yes

--- src/thread/routine.c
+++ src/thread/routine.c
@@ -6,12 +6,15 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 16:21:44 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/03/15 18:16:29 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/03/17 18:52:37 by glag             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "thread/philo.h"
 
+//lock une variable commune dans le thread principal puis l,unlock a la fin de la creation des threads
+//check erreur puis start routine
+//apres check erreur apres les mutex :(
 void	*routine(void *philo_)
 {
 	t_philo	*philo = philo_;
